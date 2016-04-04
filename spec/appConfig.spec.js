5dbefda958f3fa826ff77e2a5d771e488bb11598
David Neal <david@reverentgeek.com>
2016-04-04 15:33:04 -0400

    Adding appConfig + specs

---
+++ spec/appConfig.spec.js
@@ -0,0 +1,79 @@
+import ConfigStorage from "../src/configStorage";
+import AppConfig from "../src/appConfig";
+import fs from "fs-jetpack";
+
+describe( "App Config", () => {
+	describe( "session config", () => {
+		let appConfig;
+		let defaultConfig;
+		before( () => {
+			let store = new ConfigStorage();
+			appConfig = new AppConfig( store );
+			defaultConfig = appConfig.defaults;
+		} );
+
+		it( "should get all defaults", () => {
+			let config = appConfig.config;
+			should.exist( config );
+			let expected = defaultConfig;
+			config.should.deep.equal( expected );
+		} );
+
+		it( "should get login", () => {
+			let config = appConfig.login;
+			should.exist( config );
+			let expected = defaultConfig.login;
+			config.should.deep.equal( expected );
+		} );
+
+		it( "should set login", () => {
+			let val = appConfig.login;
+			should.exist( val );
+			let expected = defaultConfig.login;
+			val.should.deep.equal( expected );
+			appConfig.login = { host: "localhost" };
+			val = appConfig.login;
+			val.should.not.deep.equal( expected );
+		} );
+	} );
+
+	describe( "persistent config", () => {
+		let appConfig;
+		let defaultConfig;
+		let configFile = "./spec/test-files/test.app.config";
+
+		before( () => {
+			let store = new ConfigStorage( defaultConfig, true, "./spec/test-files/", "test.app.config" );
+			appConfig = new AppConfig( store );
+			defaultConfig = appConfig.defaults;
+		} );
+
+		after( () => {
+			fs.remove( configFile );
+		} );
+
+		it( "should get all defaults", () => {
+			let config = appConfig.config;
+			should.exist( config );
+			let expected = defaultConfig;
+			config.should.deep.equal( expected );
+		} );
+
+		it( "should get login", () => {
+			let config = appConfig.login;
+			should.exist( config );
+			let expected = defaultConfig.login;
+			config.should.deep.equal( expected );
+		} );
+
+		it( "should set login", () => {
+			let val = appConfig.login;
+			should.exist( val );
+			let expected = defaultConfig.login;
+			val.should.deep.equal( expected );
+			appConfig.login = { host: "localhost" };
+			val = appConfig.login;
+			val.should.not.deep.equal( expected );
+		} );
+	} );
+} );
---
+++ src/appConfig.js
@@ -0,0 +1,31 @@
+const _ = require( "lodash" );
+const defaultConfig = {
+			login: { host: "", email: "", password: "", proxy: "" }
+		};
+
+export default class AppConfig {
+	constructor( store ) {
+		this.store = store;
+	}
+
+	get config() {
+		let c = _.clone( defaultConfig );
+		let config = this.store.getAll();
+		Object.assign( c, config );
+		return c;
+	}
+	set config( obj ) {
+		this.store.setAll( obj );
+	}
+
+	get defaults() {
+		return defaultConfig;
+	}
+
+	get login() {
+		return this.store.getItem( "login", defaultConfig.login );
+	}
+	set login( value ) {
+		this.store.setItem( "login", value );
+	}
+}
